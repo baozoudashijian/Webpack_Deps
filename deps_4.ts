00b2b18ab9dd85c5b3e07c4799586fcabc7418df
Roger Zhang <1787009521@qq.com>
2022-07-10 06:41:14 +0800

    feat: 解决循环依赖，调用

---
+++ deps_4.ts
@@ -0,0 +1,58 @@
+import { parse } from '@babel/parser'
+import traverse from '@babel/traverse'
+import { readFileSync } from 'fs'
+import { resolve, relative, dirname } from 'path'
+
+// 运行代码：node -r ts-node/register --inspect-brk let_to_var.ts
+const projectRoot = resolve(__dirname, 'project_4') // /Users/roger/project/Webpack_Deps/project_1/project_1
+
+// 定义依赖关系模型
+type DepRelation = {
+    [key: string]: {
+        deps: string[],
+        code: string
+    }
+}
+
+// 初始化一个空的depRelation，用于搜集依赖
+const depRelation: DepRelation = {}
+
+colleCodeAndDeps(resolve(projectRoot,'index.js'))
+
+console.log(depRelation)
+
+function colleCodeAndDeps(filepath: string) {
+    const key = getProjectPath(filepath) // index.js
+
+    if(Object.keys(depRelation).includes(key)) {
+        return
+    }
+    
+    const code = readFileSync(filepath).toString() // 读取index.js文件内容
+
+    // 初始化depRelation
+    depRelation[key] = {deps: [], code: code}
+
+    // 代码转换为ast
+    const ast = parse(code, {sourceType: 'module'})
+
+    traverse(ast, {
+        enter: path => {
+            if(path.node.type === 'ImportDeclaration') {
+                // 转成绝对路径
+                const depAbsolutePath = resolve(dirname(filepath), path.node.source.value) // path.node.source.value等于./b.js是相对路径
+                
+                // 转化成相对于根路径目录
+                const depProjectPath = getProjectPath(depAbsolutePath)
+
+                depRelation[key].deps.push(depProjectPath)
+                colleCodeAndDeps(depAbsolutePath)
+            }
+        }
+    })
+}
+
+function getProjectPath(path: string) {
+    return relative(projectRoot, path)
+}
+
---
+++ project_4/b.js
@@ -0,0 +1,7 @@
+import a from './a.js';
+
+const b = {
+    value: a.value + 1
+}
+
+export default b
